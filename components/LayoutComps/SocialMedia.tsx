28f6741376f695b3c9727147f84137d08074c163
Tomás Macía <tomimacia@gmail.com>
2023-05-29 01:07:18 -0300

    Styled social media

--- components/LayoutComps/SocialMedia.tsx
+++ components/LayoutComps/SocialMedia.tsx
@@ -1,16 +1,25 @@
-import { Flex, Text } from "@chakra-ui/react";
-import Link from "next/link";
-import Typewriter from "typewriter-effect";
-import { MailData, SocialMediaData } from "../../data/SocialMediaData";
 import { useLanguage } from "@/context/languageContext";
+import { Flex, Text, useColorModeValue } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { useState } from "react";
+import Typewriter from "typewriter-effect";
+import {
+  ContactData,
+  ContactText,
+  SocialMediaData,
+} from "../../data/SocialMediaData";
 export const SocialMedia = () => {
+  const [currentType, setCurrentType] = useState("Instagram");
   const language = useLanguage();
+  const colorMode = useColorModeValue(0, 1);
+  const baseColor = useColorModeValue("#aaa", "#fff");
   return (
     <Flex gap={3} flexDir="column">
       <Flex mt={2} w="100%" align="center" justify="space-between">
-        <Flex gap={[4,5,6,7]}>
+        <Flex gap={[4, 5, 6, 7]}>
           {SocialMediaData.map((media) => {
-            const { href, title, MediaIcon } = media;
+            const { href, iconColor, title, MediaIcon } = media;
             return (
               <Link
                 href={href}
@@ -18,23 +27,72 @@ export const SocialMedia = () => {
                 rel="noreferred noopener"
                 target="_blank"
               >
-                <MediaIcon title={title} cursor="Pointer" />
+                <motion.div
+                  style={{ borderRadius: "8px" }}
+                  animate={
+                    title !== "LinkedIn" && {
+                      color:
+                        currentType === title
+                          ? iconColor[colorMode]
+                          : baseColor,
+                      boxShadow:
+                        currentType === title
+                          ? `0 0 10px ${iconColor[colorMode]}`
+                          : `0 0 0px ${iconColor[colorMode]}`,
+                    }
+                  }
+                >
+                  <MediaIcon
+                    title={title}
+                    color={
+                      title === "LinkedIn" ? iconColor[colorMode] : undefined
+                    }
+                    cursor="Pointer"
+                  />
+                </motion.div>
               </Link>
             );
           })}
         </Flex>
-        <Flex align="center" gap={2}>
-          <Link href={MailData.href} rel="noreferred noopener" target="_blank">
-            <MailData.MediaIcon title={MailData.title} cursor="pointer" />
-          </Link>
-          <Text>{MailData.text[language]}</Text>
+        <Flex align="center" gap={4}>
+          {ContactData.map((data) => {
+            return (
+              <Link
+                href={data.href}
+                key={data.href}
+                rel="noreferred noopener"
+                target="_blank"
+              >
+                <data.MediaIcon
+                  style={{ color: data.iconColor[colorMode] }}
+                  title={data.title}
+                  cursor="pointer"
+                />
+              </Link>
+            );
+          })}
+          <Text>{ContactText[language]}</Text>
         </Flex>
       </Flex>
       <Flex>
         <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("instagram")
+              .pauseFor(2500)
+              .deleteAll()
+              .callFunction(() => setCurrentType("Github"))
+              .typeString("github")
+              .pauseFor(2500)
+              .deleteAll()
+              .callFunction(() => setCurrentType("Twitter"))
+              .typeString("twitter")
+              .pauseFor(2500)
+              .deleteAll()
+              .callFunction(() => setCurrentType("Instagram"))
+              .start();
+          }}
           options={{
-            strings: ["instagram", "github", "twitter"],
-            autoStart: true,
             loop: true,
           }}
         />
--- data/SocialMediaData.tsx
+++ data/SocialMediaData.tsx
@@ -11,33 +11,42 @@ export const SocialMediaData = [
   {
     href: "https://www.instagram.com/tomimacia",
     title: "Instagram",
+    iconColor: ["#5851DB", "#C13584"],
     MediaIcon: BsInstagram,
   },
   {
     href: "https://github.com/tomimacia/",
     title: "Github",
+    iconColor: ["#171515", "#dddddd"],
     MediaIcon: BsGithub,
   },
   {
     href: "https://www.twitter.com/tomimacia",
     title: "Twitter",
+    iconColor: ["#00acee", "#00acee"],
     MediaIcon: BsTwitter,
-  },
-  {
-    href: "https://wa.me/541161685995",
-    title: "WhatsApp",
-    MediaIcon: BsWhatsapp,
   },
   {
     href: "https://www.linkedin.com/in/tomas-eduardo-macia/",
     title: "LinkedIn",
+    iconColor: ["#0072b1", "#0072b1"],
     MediaIcon: BsLinkedin,
   },
 ];
 
-export const MailData = {
-  href: "mailto: tomasemacia@gmail.com",
-  title: "Mail",
-  MediaIcon: BsEnvelope,
-  text: { English: "Get in touch!", Español: "Contáctame!" },
-};
+export const ContactData = [
+  {
+    href: "https://wa.me/541161685995",
+    iconColor: ["#075E54", "#25D366"],
+    title: "WhatsApp",
+    MediaIcon: BsWhatsapp,
+  },
+  {
+    href: "mailto: tomasemacia@gmail.com",
+    iconColor: ["#808080", "#e3e3e3"],
+    title: "Mail",
+    MediaIcon: BsEnvelope,
+  },
+];
+
+export const ContactText = { English: "Get in touch!", Español: "Contáctame!" };
