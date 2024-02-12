cd767f62d903cd5ad698a3fdfe49788e4e175c68
Tomas <tomimacia@gmail.com>
2024-02-12 12:03:32 -0300

    Manzo adde to work

--- components/Work/WorkCard.tsx
+++ components/Work/WorkCard.tsx
@@ -1,7 +1,6 @@
-import { Button, Divider, Flex, Heading, Text } from "@chakra-ui/react";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import { Button, Divider, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import Link from 'next/link';
+import React from 'react';
 type WorkCardType = {
   title: string;
   description: string;
@@ -18,56 +17,57 @@ const WorkCard = ({
 }: WorkCardType) => {
   return (
     <Flex
-      borderRadius="10px"
+      borderRadius='10px'
       p={[0.5, 1, 2, 3]}
-      bg="whiteAlpha.500"
+      bg='whiteAlpha.500'
       gap={3}
-      flexDir="column"
+      flexDir='column'
       pb={1}
     >
       <Flex
-        flexDir={["column", "column", "row", "row"]}
-        align="center"
-        justify="space-around"
+        flexDir={['column', 'column', 'row', 'row']}
+        align='center'
+        justify='space-around'
         gap={2}
       >
-        <Heading as="h3" fontSize="2xl">
+        <Heading as='h3' fontSize='2xl'>
           {title}
         </Heading>
         <Image
-          alt={title.split(" ").join("-")}
+          alt={title.split(' ').join('-')}
           src={IMG}
-          height={150}
-          width={150}
+          height={70}
+          width='auto'
+          maxW={150}
           style={{
-            objectPosition: "center",
-            objectFit: "contain",
-            borderRadius: "3px",
+            objectPosition: 'center',
+            objectFit: 'contain',
+            borderRadius: '3px',
           }}
         />
       </Flex>
-      <Divider borderColor="gray" w="90%" mx="auto" />
+      <Divider borderColor='gray' w='90%' mx='auto' />
       <Flex
-        flexDir="column"
+        flexDir='column'
         p={1}
         pb={2}
-        bg="blackAlpha.50"
-        borderRadius="10px"
+        bg='blackAlpha.50'
+        borderRadius='10px'
       >
         <Text
-          fontSize={["sm", "sm", "md", "md"]}
+          fontSize={['sm', 'sm', 'md', 'md']}
           p={1}
           dangerouslySetInnerHTML={{ __html: description }}
         />
         <Button
-          bg="blue.400"
-          size={["xs", "xs", "sm", "sm"]}
-          alignSelf="center"
+          bg='blue.400'
+          size={['xs', 'xs', 'sm', 'sm']}
+          alignSelf='center'
           as={Link}
           href={href}
-          _hover={{ bg: "blue.200" }}
-          target="_blank"
-          rel="noreferred noopener"
+          _hover={{ bg: 'blue.200' }}
+          target='_blank'
+          rel='noreferred noopener'
           my={2}
         >
           {buttonText}
--- components/Work/WorkList.tsx
+++ components/Work/WorkList.tsx
@@ -1,14 +1,15 @@
-import { Flex } from "@chakra-ui/react";
-import React from "react";
-import WorkCard from "./WorkCard";
-import EspacioSanMartinIMG from "@/public/images/Work/EspacioSanMartin.jpg";
-import ConsultorioDepierre from "@/public/images/Work/ConsultorioDepierre.jpg";
-import { LanguageStateType } from "@/types/LanguageType";
+import { Flex } from '@chakra-ui/react';
+import React from 'react';
+import WorkCard from './WorkCard';
+import EspacioSanMartinIMG from '@/public/images/Work/EspacioSanMartin.jpg';
+import CarniceriasManzo from '@/public/images/Work/CarnieceriasManzo.png';
+import ConsultorioDepierre from '@/public/images/Work/ConsultorioDepierre.jpg';
+import { LanguageStateType } from '@/types/LanguageType';
 
 const WorkList = ({ language }: { language: LanguageStateType }) => {
   const WorkList = [
     {
-      title: "Consultorio Depierre",
+      title: 'Consultorio Depierre',
       description: {
         English: `The Medical Office of <strong>Dr. Pablo David Depierre</strong> provides specialized and welcoming cardiological care in Lomas de Zamora.<br/> <strong>Dr. Depierre</strong> leads a dedicated team committed to heart health. We conduct studies to diagnose and treat cardiovascular diseases.
             `,
@@ -17,10 +18,10 @@ const WorkList = ({ language }: { language: LanguageStateType }) => {
             `,
       },
       IMG: ConsultorioDepierre.src,
-      href: "https://www.consultoriodrdepierre.com.ar/",
+      href: 'https://www.consultoriodrdepierre.com.ar/',
     },
     {
-      title: "Espacio San Martín",
+      title: 'Espacio San Martín',
       description: {
         English: `<strong>Espacio San Martín</strong> is a center dedicated to the education and support of the community. They offer courses, free legal advice, and enriching activities to promote the personal and professional development of their neighbors.
             `,
@@ -28,12 +29,21 @@ const WorkList = ({ language }: { language: LanguageStateType }) => {
             `,
       },
       IMG: EspacioSanMartinIMG.src,
-      href: "https://www.sanmartinjuancruz.com.ar/",
+      href: 'https://www.sanmartinjuancruz.com.ar/',
+    },
+    {
+      title: 'Carnicerías Manzo',
+      description: {
+        English: `<strong>Carnicerías Manzo</strong> is a company specializing in high-quality meat with over 60 years of experience. It offers a wide variety of premium cuts and fresh meat products, renowned for its excellence and commitment to customer satisfaction.`,
+        Español: `<strong>Carnicerías Manzo</strong> es una empresa familiar especializada en carne de alta calidad con más de 60 años de experiencia. Ofrece una amplia variedad de cortes premium y productos cárnicos frescos, destacándose por su excelencia y compromiso con la satisfacción del cliente.`,
+      },
+      IMG: CarniceriasManzo.src,
+      href: 'https://www.carniceriasmanzo.com.ar/',
     },
   ];
 
   return (
-    <Flex gap={5} p={[1, 1, 2, 2]} flexDir="column">
+    <Flex gap={5} p={[1, 1, 2, 2]} flexDir='column'>
       {WorkList.map((item) => {
         const { title, description, IMG, href } = item;
         return (
@@ -43,7 +53,7 @@ const WorkList = ({ language }: { language: LanguageStateType }) => {
             description={description[language]}
             IMG={IMG}
             href={href}
-            buttonText={language === "Español" ? "Visitar" : "Visit site"}
+            buttonText={language === 'Español' ? 'Visitar' : 'Visit site'}
           />
         );
       })}
