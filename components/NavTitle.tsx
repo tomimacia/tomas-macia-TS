afcef3e3c61ef205d23e15f198867677db2b6057
Tomas <tomimacia@gmail.com>
2024-12-19 02:08:21 -0300

    New description

--- components/NavTitle.tsx
+++ components/NavTitle.tsx
@@ -1,22 +1,20 @@
-import { Box, Container, Flex, Heading, Image, Text } from "@chakra-ui/react";
-import NavProfileImage from "../public/images/ProfileImages/NavProfileImage.jpg";
-import { SocialMedia } from "./LayoutComps/SocialMedia";
+import { Box, Container, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import NavProfileImage from '../public/images/ProfileImages/NavProfileImage.jpg';
+import { SocialMedia } from './LayoutComps/SocialMedia';
 const NavTitle = () => {
   return (
     <Container mt={5} p={3}>
-      <Flex justify="space-between">
+      <Flex justify='space-between'>
         <Box>
-          <Heading w="100%">
-            Tomás Macía
-          </Heading>
-          <Text w="100%" fontSize="lg" fontWeight="bold">
-            Full-stack React & Node developer
+          <Heading w='100%'>Tomás Macía</Heading>
+          <Text w='100%' fontSize='lg' fontWeight='bold'>
+            React & Next JS developer
           </Text>
         </Box>
         <Image
-          borderRadius="full"
+          borderRadius='full'
           h={100}
-          alt="main-profile-img"
+          alt='main-profile-img'
           src={NavProfileImage.src}
         />
       </Flex>
