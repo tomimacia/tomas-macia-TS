69c8cf0a709ef3d9a89a5425196aec071f6d91bc
Tomas <tomimacia@gmail.com>
2025-04-15 14:50:17 -0300

    Edad variable

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,43 @@
-import { Box, Flex, Image, Text, useColorModeValue } from "@chakra-ui/react";
-import IndexData from "../data/Index.json";
-import useJsonData from "../hooks/useJsonData";
-import DescriptionProfile from "../public/images/ProfileImages/DescriptionProfile.png";
-import Layout from "../components/Layouts/Article";
-import { IndexDataType } from "@/types/Datatype";
+import { Box, Flex, Image, Text, useColorModeValue } from '@chakra-ui/react';
+import IndexData from '../data/Index.json';
+import useJsonData from '../hooks/useJsonData';
+import DescriptionProfile from '../public/images/ProfileImages/DescriptionProfile.png';
+import Layout from '../components/Layouts/Article';
+import { IndexDataType } from '@/types/Datatype';
 export default function Home() {
   const data: IndexDataType = useJsonData(IndexData);
+  const edad =
+    new Date().getFullYear() -
+    1994 -
+    (new Date().getMonth() < 8 ||
+    (new Date().getMonth() === 8 && new Date().getDate() < 15)
+      ? 1
+      : 0);
   return (
-    <Layout pageTitle={data?.Heading} headTitle="Portfolio">
+    <Layout pageTitle={data?.Heading} headTitle='Portfolio'>
       <Flex
         mt={4}
         p={2}
-        borderRadius="3%"
-        bg={useColorModeValue("blackAlpha.100", "blackAlpha.300")}
-        flexDir="column"
+        borderRadius='3%'
+        bg={useColorModeValue('blackAlpha.100', 'blackAlpha.300')}
+        flexDir='column'
       >
         <Box>
           <Image
             p={1}
-            borderRadius="5%"
-            display="block"
-            float="right"
-            alt="Tomás-Macía-profile"
-            h={["200px", "250px", "300px", "350px"]}
-            maxH={["200px", "300px", "350px", "400px"]}
+            borderRadius='5%'
+            display='block'
+            float='right'
+            alt='Tomás-Macía-profile'
+            h={['200px', '250px', '300px', '350px']}
+            maxH={['200px', '300px', '350px', '400px']}
             src={DescriptionProfile.src}
           />
-          <Text p={1} borderRadius="5px" fontSize={["sm", "md", "md", "lg"]}>
+          <Text p={1} borderRadius='5px' fontSize={['sm', 'md', 'md', 'lg']}>
             {data.Description?.map((line, i) => {
               return (
-                <span key={"Line" + i}>
-                  {line}
+                <span key={'Line' + i}>
+                  {line.replace('--AGE--', edad + '')}
                   <br />
                 </span>
               );
